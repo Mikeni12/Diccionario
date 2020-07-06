7e60ed495d2a2a88f9a5d0fd006a128ad64cc30e
mikeni <maac.1106@gmail.com>
2020-07-05 19:59:21 -0500

    fix: Modifica método para pedir permisos

--- app/src/main/java/com/mikeni/diccionario/view/MainActivity.kt
+++ app/src/main/java/com/mikeni/diccionario/view/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.mikeni.diccionario.view
 
-import android.Manifest
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
@@ -10,9 +9,9 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.mikeni.diccionario.R
@@ -34,6 +33,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+            //Check if the permission is granted or not.
+            // Settings activity never returns proper value so instead check with following method
+            if (Settings.canDrawOverlays(this)) {
+                checkOverlayPermission()
+            } else { //Permission is not available
+                Toast.makeText(
+                    this,
+                    "Draw over other app permission not available. Closing the application",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -43,7 +61,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml.r
         return when (item.itemId) {
             R.id.action_settings -> {
                 checkOverlayPermission()
@@ -58,20 +76,12 @@ class MainActivity : AppCompatActivity() {
         //This permission is by default available for API<23. But for API > 23
         //you have to ask for the permission in runtime.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.SYSTEM_ALERT_WINDOW
-                )
-            ) {
-                AlertDialog.Builder(this)
-                    .setTitle("Permiso SMS")
-                    .setMessage("La app requiere accesos para enviar un SMS")
-                    .setPositiveButton("Pregúntame") { _, _ -> requestOverlayPermission() }
-                    .setNegativeButton("No") { _, _ -> }
-                    .show()
-            } else {
-                requestOverlayPermission()
-            }
+            AlertDialog.Builder(this)
+                .setTitle("Permiso SMS")
+                .setMessage("La app requiere accesos para ejecutarse en segundo plano")
+                .setPositiveButton("Sí") { _, _ -> requestOverlayPermission() }
+                .setNegativeButton("No") { _, _ -> }
+                .show()
         } else {
             startService(Intent(this@MainActivity, OverlayService::class.java))
             finish()
